6b10869ebf98c603e7e56ba811474e03d9286866
JuliyaEduardovna <juliya.eduardovna@gmail.com>
2026-04-14 19:04:09 +0700

    Added pages

---
+++ src/pages/ContactsPage.tsx
@@ -0,0 +1,5 @@
+export default function ContactsPage(){
+    return (
+        <p>Страница контактов</p>
+    );
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+export default function HomePage(){
+    return (
+        <p>Каталог товаров</p>
+    );
+}
