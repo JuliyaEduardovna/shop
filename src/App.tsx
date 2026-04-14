5199121094383315e26f7b77f8139e42b99c2de4
JuliyaEduardovna <juliya.eduardovna@gmail.com>
2026-04-14 19:04:53 +0700

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage'
 import Header from './components/Header'
+import CartPage from './pages/CartPage'
+import AboutPage from './pages/AboutPage'
+import ContactsPage from './pages/ContactsPage'
 
 function App() {
  return(
-  <Header />
+    <>
+    <Header />
+    <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contacts' element={<ContactsPage />} />
+
+    </Routes>
+    </>
  )
 }
 
